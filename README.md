be066b0a69605a527a1388770a7f62df5c799987
MarcinDl <marcindl@hotmail.com>
2021-06-12 09:47:53 +0200

    database connect

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
 # node_mongoose_front
+
+# logowanie
+
+### gmail:
+
+* login: marcinmarcin882610@gmail.com
+* hasło:
+
+### mongo atlas:
+
+* login: marcinmarcin882610@gmail.com
+* hasło:
+
+# Baza danych
+
+### Nazwa bazy danych
+
+* myFirstDatabase
+### Database user:
+
+* login: mojanazwauzytkownika
+* hasło: mojehaslouzytkownika
---
+++ app.js
@@ -0,0 +1,48 @@
+const express = require("express");
+const app = express();
+mongoose = require("mongoose");
+
+const PORT = 3800;
+const connectionString = "mongodb+srv://mojanazwauzytkownika:mojehaslouzytkownika@cluster0.rrhsm.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+
+app.use(express.urlencoded({extend: true}));
+app.use(express.json());
+
+const Character = mongoose.model("Character", mongoose.Schema({
+    name:String,
+    age:Number,
+    rank:String
+}));
+
+
+async function run(){
+    await mongoose.connect(connectionString, {
+        useNewUrlParser: true, 
+        useUnifiedTopology: true,
+        useFindAndModify: false
+        }).then(() => {
+            console.log("Połączono z bazą danych")
+        },
+            error => {
+                console.log("Błąd połączenia: " + error)
+            }
+    )
+    await Character.collection.drop() //usunięcie kolekcji - tylko wersja dev - zeby za kazdym uruchomieniem nie dodawało mi tych samych dokumentów do bazy
+    await Character.create([
+        {name: "Marcin Dłubis", age: 32, rank: "marszałek"},
+        {name: "Jan Kowalski", age: 43, rank: "komandor"},
+        {name: "Jacek Nowak", age: 69, rank: "komandor"},
+        {name: "Jacek Kowalski", age: 23, rank: "porucznik"},
+        {name: "Aleksander Macedoński", age: 41, rank: "komandor"},
+        {name: "Marian Ozimek", age: 39, rank: "pułkownik"}
+    ])
+
+    const docs = await Character.find({rank: "komandor"})
+    console.log(docs)
+};
+
+run().catch( error => console.log(error));
+
+app.listen(PORT, () => {
+    console.log("Aplikacja działa na porcie", PORT)
+})
